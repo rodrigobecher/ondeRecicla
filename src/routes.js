c32663df381718e80a5a621953ddbf9a51b094de
rodrigo <rodrigofbecher@gmail.com>
2018-06-23 01:15:22 -0300

    ajuste no titulo do cadastro Empresa

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,8 @@ import logout from './components/cadastro/Logout.vue'
 import Home from './components/home/Home.vue';
 export const routes = [
     {path: '', name:'home', component: Home, titulo:'Home', menu: true},
-    {path: '/cadastroEmpresa', name: 'cadastro', component: Cadastro, titulo:'Cadastro', menu: true},
-    {path: '/cadastroEmpresa/:id', name: 'altera', component: Cadastro, titulo:'Cadastro', menu: false},
+    {path: '/cadastroEmpresa', name: 'cadastro', component: Cadastro, titulo:'Cadastro Empresa', menu: true},
+    {path: '/cadastroEmpresa/:id', name: 'altera', component: Cadastro, titulo:'Cadastro Empresa', menu: false},
     {path: '/cadastroResiduo', name: 'cadastroResiduo', component: CadastroResiduo, titulo:'Cadastro Resíduo', menu: true},
     {path: '/cadastroResiduo/:id', name: 'alteraResiduo', component: CadastroResiduo, titulo:'Cadastro Resíduo', menu: false},
     {path: '/cadastroCategoria', name: 'cadastroCategoria', component: CadastroCategoria, titulo:'Cadastro Categoria', menu: true},
